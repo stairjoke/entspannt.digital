e771dbde4c9ffcd756f07983f57010c907a8548b
Wenzel Massag <mail@wmassag.de>
2023-03-15 19:59:26 +0000

    redesign for home

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,16 +1,21 @@
 <?= snippet('header', ['class' => 'home']) ?>
 <main class="home">
-	<article class="CTA">
+	<article class="CTA page-opener">
 		<div>
 			<?= $page->card_text()->toBlocks() ?>
-			<p><a class="button cta" href="<?= ($page->button_url()->exists()) ? $page->button_url() : $site->Contact_Link() ?>"><?= $page->button_label() ?></a></p>
+			<p>
+				<a class="button cta" href="<?= ($page->button_url()->exists()) ? $page->button_url() : $site->Contact_Link() ?>">
+					<svg class="icon inline">
+						<use href="/assets/iconSprite.svg#letter-heart"></use>
+					</svg><?= $site->contact_cta() ?>
+				</a>
+			</p>
 		</div>
 	</article>
 <?php foreach(page('projects')->children()->listed() as $project): ?>
-	<article><a class="card" href="<?= $project->url() ?>">
-		<h3><?= $project->title() ?></h3>
+	<article><a class="card" href="<?= $project->url() ?>" style="background-image: url('<?= $project->image()->url() ?>')">
 		<div class=card-body>
-			<?= $project->image() ?>
+			<h2><?= $project->title() ?></h2>
 			<p><?= $project->tagline() ?></p>
 		</div>
 	</a></article>
