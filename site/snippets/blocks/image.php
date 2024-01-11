3aa8df98fc4be44a3a009e76a567662625292086
Wenzel Massag <mail@wmassag.de>
2024-01-11 17:40:15 +0100

    responsive image block

--- site/snippets/blocks/image.php
+++ site/snippets/blocks/image.php
@@ -6,105 +6,38 @@
 
 <picture <?= ($block->class()->isNotEmpty()) ? "class='" . preg_replace("/, /", " ", $block->class()) . "'" : "" ; ?>>
 	<?php
-		// Mobile + Mobile @3x ––– DARK MODE
-		if($block->imageMobileDark()->isNotEmpty()) :
-			$srcset = ($block->imageMobileHiDPIDark()-isNotEmpty()) ? $block->imageMobileHiDPIDark()->toFile()->url() . " 3x, " : "";
-			$srcset .= $block->imageMobileDark()->toFile()->url() . " 1x";
-	?>
-	<source media="(max-width: 413px) and (prefers-color-scheme: dark)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
-	<?php
-		// Mobile + Mobile @3x
+		function generator($block, $image, $HiDPIImage, $HiDPIFactor, $mediaString){
+			$srcset = "";
+			if($image = $block->$image()->toFile()) {
+				if($imageHiDPI = $block->$HiDPIImage()->toFile()){
+					$srcset .= $imageHiDPI->url() . ' ' . $HiDPIFactor . 'x, ';
+				}
+				$srcset .= $image->url() . ' 1x';
+				
+				echo("<source media='$mediaString' srcset='$srcset' />");
+			}
+		}
 		
-		if($block->imageMobile()->isNotEmpty()) :
-			$srcset = ($block->imageMobileHiDPI()-isNotEmpty()) ? $block->imageMobileHiDPI()->toFile()->url() . " 3x, " : "";
-			$srcset .= $block->imageMobile()->toFile()->url() . " 1x";
+		function srcset($block, $name, $HiDPIFactor, $minWidth) {
+			$defaultImage = "image" . $name;
+			$HiDPIImage = "image" . $name . "hidpi";
+			$darkImage = "image" . $name . "dark";
+			$darkHiDPIImage = "image" . $name . "hidpidark";
+			
+			$media = '(min-width: '.$minWidth.'px)';
+			$mediaDark = $media . ' and (prefers-color-scheme: dark)';
+			
+			generator($block, $darkImage, $darkHiDPIImage, $HiDPIFactor, $mediaDark);
+			generator($block, $defaultImage, $HiDPIImage, $HiDPIFactor, $media);
+		}
+	
+		srcset($block, "desktopxl", "2", "1280");
+		srcset($block, "desktop", "2", "744");
+		srcset($block, "tablet", "3", "414");
+		srcset($block, "mobile", "3", "0");
 	?>
-	<source media="(max-width: 413px)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
-	
-	
-	
-	<?php
-		// Tablet + Tablet @3x –––DARK MODE
-		if($block->imageTabletDark()->isNotEmpty()) :
-			$srcset = ($block->imageTabletHiDPIDark()-isNotEmpty()) ? $block->imageTabletHiDPIDark()->toFile()->url() . " 3x, " : "";
-			$srcset .= $block->imageTabletDark()->toFile()->url() . " 1x";
-	?>
-	<source media="(min-width: 414px) and (prefers-color-scheme: dark)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
-	<?php
-		// Tablet + Tablet @3x
-		if($block->imageTablet()->isNotEmpty()) :
-			$srcset = ($block->imageTabletHiDPI()-isNotEmpty()) ? $block->imageTabletHiDPI()->toFile()->url() . " 3x, " : "";
-			$srcset .= $block->imageTablet()->toFile()->url() . " 1x";
-	?>
-	<source media="(min-width: 414px)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
-	
-	
-	
-	<?php
-		// Desktop + Desktop @2x –––DARK MODE
-		if($block->imageDesktopDark()->isNotEmpty()) :
-			$srcset = ($block->imageDesktopHiDPIDark()->isNotEmpty()) ? $block->imageDesktopHiDPIDark()->toFile()->url() . " 2x, " : "";
-			$srcset .= $block->imageDesktopDark()->toFile()->url() . " 1x";
-	?>
-	<source media="(min-width: 744px) and (prefers-color-scheme: dark)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
-	<?php
-		// Desktop + Desktop @2x
-		if($block->imageDesktop()->isNotEmpty()) :
-			$srcset = ($block->imageDesktopHiDPI()->isNotEmpty()) ? $block->imageDesktopHiDPI()->toFile()->url() . " 2x, " : "";
-			$srcset .= $block->imageDesktop()->toFile()->url() . " 1x";
-	?>
-	<source media="(min-width: 744px)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
-	
-	
-	
-	<?php
-		// Desktop XL + Desktop XL @2x ––– DARK MODE
-		if($block->imageDesktopXLDark()->isNotEmpty()) :
-			$srcset = ($block->imageDesktopXLHiDPIDark()-isNotEmpty()) ? $block->imageDesktopXLHiDPIDark()->toFile()->url() . " 2x, " : "";
-			$srcset .= $block->imageDesktopXLDark()->toFile()->url() . " 1x";
-	?>
-	<source media="(min-width: 1280px) and (prefers-color-scheme: dark)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
-		<?php
-		// Desktop XL + Desktop XL @2x
-		if($block->imageDesktopXL()->isNotEmpty()) :
-			$srcset = ($block->imageDesktopXLHiDPI()-isNotEmpty()) ? $block->imageDesktopXLHiDPI()->toFile()->url() . " 2x, " : "";
-			$srcset .= $block->imageDesktopXL()->toFile()->url() . " 1x";
-	?>
-	<source media="(min-width: 1280px)" srcset="<?= $srcset ?>" />
-	<?php
-		endif;
-	?>
-	
 	<img
 		<?= ($block->class()->isNotEmpty()) ? "class='" . preg_replace("/, /", " ", $block->class()) . "'" : "" ; ?>
 		alt="<?= $block->alt() ?>"
-		src="<?= $block->imageDesktop()->toFile()->url() ?>" />
+		src="<?= $block->imageDesktop()?->toFile()->url() ?>" />
 </picture>
