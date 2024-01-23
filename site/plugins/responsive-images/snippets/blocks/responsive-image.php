8d332631de4267912b3b4adf4395928f4673abee
Wenzel Massag <mail@wmassag.de>
2024-01-23 20:35:45 +0100

    major fix, cannot use functions in snippets

--- site/plugins/responsive-images/snippets/blocks/responsive-image.php
+++ site/plugins/responsive-images/snippets/blocks/responsive-image.php
@@ -6,76 +6,93 @@
 
 <picture <?= ($block->class()->isNotEmpty()) ? "class='" . preg_replace("/, /", " ", $block->class()) . "'" : "" ; ?>>
 	<?php
-		function responsiveImage_render($block){
-			
-			// In $config['targets'] the order must be from largest to smallest min-width!
-			
-			$config = [
-				'darkModeSupport' => true,
-				'breakpoints' => [
-					'desktop' => [
-						'min-width' => 1024,
-						'factors' => [1, 2]
-					],
-					'tablet' => [
-						'min-width' => 535,
-						'factors' => [1, 2, 3]
-					],
-					'mobile' => [
-						'min-width' => 0,
-						'factors' => [1, 3]
-					]
+		$responsiveImage_config = [
+			'breakpoints' => [
+				'desktop' => [
+					'min-width' => 1024,
+					'factors' => [1, 2]
+				],
+				'tablet' => [
+					'min-width' => 535,
+					'factors' => [1, 2, 3]
+				],
+				'mobile' => [
+					'min-width' => 0,
+					'factors' => [1, 3]
 				]
-			];
+			]
+		];
+		
+		// Iterate through all breakpoints
+		foreach($responsiveImage_config['breakpoints'] as $device => $breakpoint){
+// –– DARK MODE ––
 			
-			function responsiveImage_source($block, $device, $breakpoint, $dark = false){
-				//Open <source />-element
-				echo('<source media="(min-width: '. $breakpoint['min-width'] .'px)" ');
-				$try = $device . '1x';
+			//Open <source />-element
+			echo('<source media="(min-width: '. $breakpoint['min-width'] .'px) and (prefers-color-scheme: dark)" ');
+			$try = $device . '1x' . 'dark';
+			if($image = $block->$try()->toFile()){
+				echo('width="' . $image->width() . '" height="' . $image->height() . '" ');
+			}
+			echo('srcset="');
+			
+			// Count numebr of factors in $config array for this breakpoint
+			$numberOfFactors = 0;
+			$numberOfFactors = count($breakpoint['factors']);
+			
+			// Iterate through all factors
+			foreach($breakpoint['factors'] as $factor) {
+				// Count down how many are left
+				$numberOfFactors--;
+				
+				// Test if an image for this breakpoint and factor exists
+				$try = $device . $factor . 'x' . 'dark';
 				if($image = $block->$try()->toFile()){
-					echo('width="' . $image->width() . '" height="' . $image->height() . '" ');
-				}
-				echo('srcset="');
-				
-				// Count numebr of factors in $config array for this breakpoint
-				$numberOfFactors = 0;
-				$numberOfFactors = count($breakpoint['factors']);
-				
-				// Iterate through all factors
-				foreach($breakpoint['factors'] as $factor) {
-					// Count down how many are left
-					$numberOfFactors--;
-					
-					// Test if an image for this breakpoint and factor exists
-					$try = $device . $factor . 'x';
-					if($dark){
-						$try .= 'dark';
+			
+					// If an image exists, echo the URL followed by the factor.
+					echo($image->url() . ' ' . $factor . 'x');
+					if($numberOfFactors > 0) {
+						echo(', ');
 					}
-					if($image = $block->$try()->toFile()){
-				
-						// If an image exists, echo the URL followed by the factor.
-						echo($image->url() . ' ' . $factor . 'x');
-						if($numberOfFactors > 0) {
-							echo(', ');
-						}
-					}
-					
 				}
 				
-				echo('" />'); //end of <source />-element
 			}
 			
-			// Iterate through all breakpoints
-			foreach($config['breakpoints'] as $device => $breakpoint){
-				responsiveImage_source($block, $device, $breakpoint);
+			echo('" />'); //end of <source />-element
+			
+// ––– LIGHT MODE –––
+			
+			//Open <source />-element
+			echo('<source media="(min-width: '. $breakpoint['min-width'] .'px)" ');
+			$try = $device . '1x';
+			if($image = $block->$try()->toFile()){
+				echo('width="' . $image->width() . '" height="' . $image->height() . '" ');
+			}
+			echo('srcset="');
+			
+			// Count numebr of factors in $config array for this breakpoint
+			$numberOfFactors = 0;
+			$numberOfFactors = count($breakpoint['factors']);
+			
+			// Iterate through all factors
+			foreach($breakpoint['factors'] as $factor) {
+				// Count down how many are left
+				$numberOfFactors--;
 				
-				if($config['darkModeSupport'] === true){
-					responsiveImage_source($block, $device, $breakpoint, true);
+				// Test if an image for this breakpoint and factor exists
+				$try = $device . $factor . 'x';
+				if($image = $block->$try()->toFile()){
+			
+					// If an image exists, echo the URL followed by the factor.
+					echo($image->url() . ' ' . $factor . 'x');
+					if($numberOfFactors > 0) {
+						echo(', ');
+					}
 				}
+				
 			}
 			
+			echo('" />'); //end of <source />-element
 		}
-		responsiveImage_render($block);
 		
 		if($image = $block->desktop1x()->toFile()){
 			echo('<img alt="' . $block->alt() . '" src="' . $image->url() . '" width="' . $image->width() . '" height="' . $image->height() . '" />');
