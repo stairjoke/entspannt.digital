5f78a13b254851c2cd35df71676073185fd5409b
Wenzel Massag <mail@wmassag.de>
2024-02-18 12:01:19 +0100

    added support for alternate page titles

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,6 +1,6 @@
 <?= snippet('header') ?>
 <main>
-	<div class="headline-container"><h1><?= $page->title() ?></h1></div>
+	<div class="headline-container"><h1><?= ($page->alttitle()->isNotEmpty()) ? $page->alttitle() : $page->title() ; ?></h1></div>
 	<?= $page->main_content()->toBlocks() ?>
 </main>
 <?= snippet('footer') ?>
