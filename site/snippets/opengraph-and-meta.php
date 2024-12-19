ecb2d1320f59f4799ecfb4d6c7de2f578a7f6c1f
stairjoke <wenzel@wenzels.design>
2024-12-19 23:05:21 +0100

    default opengraph data and css in default template

---
+++ site/snippets/opengraph-and-meta.php
@@ -0,0 +1,13 @@
+<meta property="og:type" content="business.business">
+<meta property="og:title" content="Entspannt.Digital">
+<meta property="og:url" content="https://entspannt.digital">
+
+<meta property="business:contact_data:street_address" content="Imhofstr. 17">
+<meta property="business:contact_data:locality" content="Schwäbisch Gmünd">
+<meta property="business:contact_data:region" content="BW">
+<meta property="business:contact_data:postal_code" content="73525">
+<meta property="business:contact_data:country_name" content="Germany">
+
+<meta property="og:image" content="/assets/opengraph-default.jpeg">
+<meta property="og:description" content="<?= $site->description() ?>">
+<meta name="description" content="<?= $site->description() ?>">
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,12 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title><?= $site->title() ?></title>
+    <?= css('assets/css/styles.css', '@auto') ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= snippet('opengraph-and-meta') ?>
+  </head>
+  <body>
+  </body>
+</html>
