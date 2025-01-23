6a0760657d78e4d929c5d8726368eeb2e92a9322
stairjoke <wenzel@wenzels.design>
2025-01-23 20:37:00 +0100

    WIP—empty page with header and footer

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,11 +2,15 @@
 <html lang="en">
   <head>
     <title><?= $site->title() ?></title>
+    <!--<meta http-equiv="refresh" content="1">-->
     <?= css('assets/css/styles.css', '@auto') ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= snippet('opengraph-and-meta') ?>
   </head>
   <body>
+    <?= snippet('header'); ?>
+    <main></main>
+    <?= snippet('footer'); ?>
   </body>
 </html>
